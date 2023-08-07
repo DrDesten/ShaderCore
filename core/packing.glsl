906774101e4241f3c9fe6d6c37b587d1df9f81d5
DrDesten <>
2023-08-07 20:16:22 +0200

    renaming

--- core/packing.glsl
+++ core/packing.glsl
@@ -10,7 +10,7 @@ const float INT16_SCALE_INV = 1 / INT16_SCALE;
 const float INT24_SCALE     = 16777215;
 const float INT24_SCALE_INV = 1 / INT24_SCALE;
 
-vec3 Int24ToVec3_asfloat(float x) {
+vec3 i24ToVec3_f(float x) {
     int ix = int(x * INT24_SCALE);
     return vec3(
         ix & 255,          // Bitwise AND. Masks the first 8 bits (255 -> 11111111 in binary, AND operation zeros out all other bits)
@@ -18,7 +18,7 @@ vec3 Int24ToVec3_asfloat(float x) {
         (ix >> 16) & 255   // Same principle
     ) * INT8_SCALE_INV;
 }
-vec3 Int24ToVec3(int x) {
+vec3 i24ToVec3(int x) {
     return vec3(
         x & 255,          // Bitwise AND. Masks the first 8 bits (255 -> 11111111 in binary, AND operation zeros out all other bits)
         (x >> 8)  & 255,  // Bitshift down by 8. Moves the first 8 bits out. Afterwards, selecting the first 8 bits again (this isolates bit 9-16)
@@ -26,15 +26,14 @@ vec3 Int24ToVec3(int x) {
     ) * INT8_SCALE_INV;
 }
 
-
-vec2 Int16ToVec2_asfloat(float x) {
+vec2 i16ToVec2_f(float x) {
     int ix = int(x * INT16_SCALE);
     return vec2(
         ix & 255,        // Bitwise AND. Masks the first 8 bits (255 -> 11111111 in binary, AND operation zeros out all other bits)
         (ix >> 8) & 255  // Bitshift down by 8. Moves the first 8 bits out. Afterwards, selecting the first 8 bits again (this isolates bit 9-16)
     ) * INT8_SCALE_INV;
 }
-vec2 Int16ToVec2(int x) {
+vec2 i16ToVec2(int x) {
     return vec2(
         x & 255,         // Bitwise AND. Masks the first 8 bits (255 -> 11111111 in binary, AND operation zeros out all other bits)
         (x >> 8) & 255   // Bitshift down by 8. Moves the first 8 bits out. Afterwards, selecting the first 8 bits again (this isolates bit 9-16)
@@ -43,20 +42,20 @@ vec2 Int16ToVec2(int x) {
 
 
 
-int Vec3ToInt24(vec3 x) {
+int vec3ToI24(vec3 x) {
     ivec3 ix = ivec3(x * INT8_SCALE);
     return ix.x + (ix.y << 8) + (ix.z << 16);
 }
-float Vec3ToInt24_asfloat(vec3 x) {
+float vec3ToI24_f(vec3 x) {
     ivec3 ix = ivec3(x * INT8_SCALE);
     return float( ix.x + (ix.y << 8) + (ix.z << 16) ) * INT24_SCALE_INV;
 }
 
-int Vec2ToInt16(vec2 x) {
+int vec2ToI16(vec2 x) {
     ivec2 ix = ivec2(x * INT8_SCALE);
     return ix.x + (ix.y << 8);
 }
-float Vec2ToInt16_asfloat(vec2 x) {
+float vec2ToI16_f(vec2 x) {
     ivec2 ix = ivec2(x * INT8_SCALE);
     return float( ix.x + (ix.y << 8) ) * INT16_SCALE;
 }
--- core/tonemap.glsl
+++ core/tonemap.glsl
@@ -1,32 +1,32 @@
 #if !defined CORE_TONEMAP
 #define CORE_TONEMAP
 
-vec3 reinhard_tonemap(vec3 color, float a) {
+vec3 tm_reinhard(vec3 color, float a) {
     return color / (a + color);
 }
-vec3 reinhard_luminance_tonemap(vec3 color, float a) {
+vec3 tm_reinhard_luminance(vec3 color, float a) {
     float l = luminance(color);
     return color / (a+l);
 }
-vec3 reinhard_jodie_tonemap(vec3 color, float a) {
+vec3 tm_reinhard_jodie(vec3 color, float a) {
     float l   = luminance(color);
     vec3 tmc  = color / (color + a);
     return mix(color / (l+a), tmc, tmc);
 }
-vec3 reinhard_sqrt_tonemap(vec3 color, float a) {
+vec3 tm_reinhard_sqrt(vec3 color, float a) {
     return color / sqrt(color * color + a);
 }
-vec3 reinhard_sqrt_tonemap_inverse(vec3 color, float a) {
+vec3 tm_reinhard_sqrt_inverse(vec3 color, float a) {
     return sqrt(a) * color * inversesqrt(-color * color + 1);
 }
 
 
-vec3 unreal_tonemap(vec3 color) {
+vec3 tm_unreal(vec3 color) {
   return color / (color + 0.155) * 1.019;
 }
 
 
-vec3 exp_tonemap(vec3 color, float a) {
+vec3 tm_exp(vec3 color, float a) {
     return 1 - exp(-color * a);
 }
 
