2e4bc3f645b3c46e9cf016ee601267598dc3ebc4
DrDesten <>
2023-08-31 22:25:44 +0200

    stuff

--- core/random.glsl
+++ core/random.glsl
@@ -16,16 +16,16 @@ vec2 rand2(vec2 x) {
     float t = rand(x);
     return vec2(t, rand(t * 50 - 25));
 }
-
+        
 float noise(vec2 x) {
     vec2 i = floor(x);
     vec2 f = fract(x);
 
 	// Four corners in 2D of a tile
 	float a = rand(i);
-    float b = rand(i + vec2(1.0, 0.0));
-    float c = rand(i + vec2(0.0, 1.0));
-    float d = rand(i + vec2(1.0, 1.0));
+    float b = rand(i + vec2(1,0));
+    float c = rand(i + vec2(0,1));
+    float d = rand(i + vec2(1,1));
 
     vec2 u = f * f * (3.0 - 2.0 * f);
 	return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
@@ -42,12 +42,12 @@ float noise(float x) {
 }
 
 float fbm(vec2 x, int n) {
-	float v = 0.0;
-	float a = 0.5;
+	float v = .0;
+	float a = .5;
 	vec2 shift = vec2(100);
 
 	// Rotate to reduce axial bias
-    const mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
+    const mat2 rot = mat2(cos(.5), sin(.5), -sin(.5), cos(.5));
 
 	for (int i = 0; i < n; ++i) {
 		v += a * noise(x);
@@ -58,12 +58,12 @@ float fbm(vec2 x, int n) {
 }
 
 float fbm(vec2 x, int n, float scale, float falloff) {
-	float v = 0.0;
-	float a = 0.5;
+	float v = .0;
+	float a = .5;
 	vec2 shift = vec2(100);
 
 	// Rotate to reduce axial bias
-    const mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
+    const mat2 rot = mat2(cos(.5), sin(.5), -sin(.5), cos(.5));
 
 	for (int i = 0; i < n; ++i) {
 		v += a * noise(x);
