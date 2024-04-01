1fb45532ea2e1295a24568862ccfd3f61e2a73b6
DrDesten <>
2024-04-01 14:52:26 +0200

    added distant horizons uniforms

---
+++ dh/uniforms.glsl
@@ -0,0 +1,16 @@
+#ifndef CORE_DH_UNIFORMS
+#define CORE_DH_UNIFORMS
+
+#if defined IS_IRIS && defined DISTANT_HORIZONS
+
+uniform float dhNearPlane;
+uniform float dhFarPlane;
+uniform int dhRenderDistance;
+
+uniform mat4 dhProjection;
+uniform mat4 dhProjectionInverse;
+uniform mat4 dhPreviousProjection;
+
+#endif
+
+#endif
