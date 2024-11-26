34c41214edd0bc18766970fe892083f894ebf7a4
DrDesten <>
2024-11-26 22:25:22 +0100

    stuff

--- dh/textures.glsl
+++ dh/textures.glsl
@@ -4,7 +4,7 @@
 #ifdef DISTANT_HORIZONS
 
 uniform sampler2D dhDepthTex0;
-uniform sampler2D dhDepthTex1;
+//uniform sampler2D dhDepthTex1;
 
 float getDepthDH( vec2 coord ) {
     return texture(dhDepthTex0, coord).x;
